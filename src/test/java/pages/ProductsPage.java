af0ba0d6243d489f414637f519b5c2499f9494e1
Anton Sidorik <darkmank79@gmail.com>
2021-06-22 13:34:41 +0300

    Add tests for Set and Reset

--- src/test/java/pages/ProductsPage.java
+++ src/test/java/pages/ProductsPage.java
@@ -2,12 +2,17 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 public class ProductsPage extends BasePage{
 
     String addToCartButton = "//*[contains(text(), '%s')]//ancestor::*[@class='inventory_item']//child::button";
     String deleteFromCartButton = "//*[contains(text(), '%s')]//ancestor::*[@class='inventory_item']//child::button";
-    public static final By QUALITY_ITEMS = By.id("shopping_cart_container");
+    public static final By CART = By.id("shopping_cart_container");
+    public static final By SELECT = By.xpath("//select[@class='product_sort_container']");
+    public static final By RESET = By.xpath("//a[@id='reset_sidebar_link']");
+    public static final By MENU = By.xpath("//button[@id='react-burger-menu-btn']");
 
     public ProductsPage (WebDriver driver){
         super(driver);
@@ -22,10 +27,19 @@ public class ProductsPage extends BasePage{
         return driver.findElement(By.xpath(String.format(deleteFromCartButton,itemName))).getText();
     }
     public String qualityItemOnCart(){
-       return driver.findElement(QUALITY_ITEMS).getText();
+       return driver.findElement(CART).getText();
     }
     public CartPage openCartPage(){
-        driver.findElement(QUALITY_ITEMS).click();
+        driver.findElement(CART).click();
     return new CartPage(driver);
+    }
+    public void setSelect (String value){
+        WebElement selectElem = driver.findElement(SELECT);
+        Select select = new Select(selectElem);
+        select.selectByValue(value);
+    }
+    public void resetAppState(){
+        driver.findElement(MENU).click();
+        driver.findElement(RESET).click();
     }
 }
--- src/test/java/tests/ProductsTest.java
+++ src/test/java/tests/ProductsTest.java
@@ -1,7 +1,12 @@
 package tests;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.json.JsonOutput;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class ProductsTest extends BaseTest {
 
@@ -27,5 +32,25 @@ public class ProductsTest extends BaseTest {
         productsPage.openCartPage();
         cartPage.findAddedItem("Sauce Labs Backpack");
         cartPage.findAddedItem("Sauce Labs Onesie");
+    }
+    @Test
+    public void selectProductSort(){
+        loginPage.openSite("https://www.saucedemo.com/");
+        loginPage.LogIn(NAME,PASSWORD);
+        productsPage.setSelect("za");
+        List<WebElement> elements = driver.findElements(By.xpath("//div[@class='inventory_item_name']"));
+        String firstItem = elements.get(0).getText();
+        Assert.assertEquals(firstItem,"Test.allTheThings() T-Shirt (Red)","Sorting didn't work");
+    }
+    @Test
+    public void checkIfResetWork(){
+        loginPage.openSite("https://www.saucedemo.com/");
+        loginPage.LogIn(NAME,PASSWORD);
+        productsPage.addItemToCart("Sauce Labs Backpack");
+        String qualityItems1 = productsPage.qualityItemOnCart();
+        Assert.assertEquals(qualityItems1,"1","Quality Items Displayed incorrectly");
+        productsPage.resetAppState();
+        String qualityItems0 = productsPage.qualityItemOnCart();
+        Assert.assertEquals(qualityItems0,"","Reset doesn't work");
     }
 }
